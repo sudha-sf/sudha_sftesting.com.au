575384aa7173685824457fa4cac0fe4e608e163e
Gabriel <gabriel@saltandfuessel.com.au>
2016-03-08 09:42:32 +1100

    Update on projec tlisting

--- resources/views/testmate/projects-listing.blade.php
+++ resources/views/testmate/projects-listing.blade.php
@@ -3,19 +3,7 @@
 @section('content')
 <div class="col-xs-12">
     <div class="box">
-      <div class="box-header">
-        <h3 class="box-title"></h3>
 
-        <div class="box-tools">
-          <div class="input-group input-group-sm" style="width: 150px;">
-            <input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
-
-            <div class="input-group-btn">
-              <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
-            </div>
-          </div>
-        </div>
-      </div>
       <!-- /.box-header -->
       <div class="box-body table-responsive no-padding">
         <table class="table table-hover" id="projects-table">
