93f101357bf2ab3a1d667c1b61741b6e0b68b75e
Tran Nam Hoai <hoaitn@qsoft.com.vn>
2016-03-10 16:59:17 +0700

    #Task 10: Build 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>404 Not Found | Home :: TestMate</title>
+		<meta name="keywords" content="404 iphone web template, Android web template, Smartphone web template, free webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
+		<link href="/testmate/css/404.css" rel="stylesheet" type="text/css"  media="all" />
+	</head>
+	<body>
+<script async type='text/javascript' src='//cdn.fancybar.net/ac/fancybar.js?zoneid=1502&serve=C6ADVKE&placement=w3layouts' id='_fancybar_js'></script>
+
+
+		<!--start-wrap--->
+		<div class="wrap">
+			<!---start-header---->
+				<div class="header">
+					<div class="logo">
+						<h1><a>Ohh</a></h1>
+					</div>
+				</div>
+			<!---End-header---->
+			<!--start-content------>
+			<div class="content">
+				<img src="/testmate/images/error-img.png" title="error" />
+				<p><span><label>O</label>hh.....</span>You Requested the page that is no longer There.</p>
+				
+				<a href="{{url('/')}}">Back To Home</a>
+<div style="text-align: center;">
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-9153409599391170"
+     data-ad-slot="6850850687"></ins>
+</div>
+				<div class="copy-right">
+					<p>&copy; 2016 Ohh. All Rights Reserved | Design by <a href="http://www.portal.testmate.com.au/">TestMate</a></p>
+				</div>
+   			</div>
+			<!--End-Cotent------>
+		</div>
+		<!--End-wrap--->
+	</body>
+</html>
+
